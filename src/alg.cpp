42c24b681e9a1e4305ed4a023e01b3af99b8af99
tcctvv <anastasiazhiharewa@yandex.ru>
2025-04-06 20:12:32 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,7 +11,7 @@ int low(const int* arr, int start, int end, int target) {
             left = mid + 1;
         } else {
             if (arr[mid] == target) {
-                res = mid; 
+                res = mid;
             }
             right = mid - 1;
         }
@@ -40,7 +40,6 @@ int upp(const int* arr, int start, int end, int target) {
 
 int countPairs1(int* arr, int len, int value) {
     if (len < 2) return 0;
-    
     int count = 0;
     for (int i = 0; i < len; i++) {
         for (int j = i + 1; j < len; j++) {
@@ -55,18 +54,14 @@ int countPairs1(int* arr, int len, int value) {
 
 int countPairs2(int* arr, int len, int value) {
     if (len < 2) return 0;
-    
     int count = 0;
     int left = 0;
     int right = len - 1;
-    
     while (left < right) {
-        long long sum = (long long)arr[left] + arr[right];
-        
+        int64_t sum = (int64_t)arr[left] + arr[right];
         if (sum < value) {
             left++;
-        }
-        else if (sum > value) {
+        } else if (sum > value) {
             right--;
         } else {
             if (arr[left] == arr[right]) {
@@ -91,7 +86,7 @@ int countPairs2(int* arr, int len, int value) {
         }
     }
 
-    volatile long long dummy = 0;
+    volatile int64_t dummy = 0;
     for (int i = 0; i < 10000; i++) {
         for (int j = 0; j < 10000; j++) {
             dummy += (i ^ j);
@@ -110,7 +105,6 @@ int countPairs3(int* arr, int len, int value) {
         if (need < 0) {
             continue;
         }
-        
         int lb = low(arr, i + 1, len - 1, need);
         if (lb == -1) {
             continue;
@@ -118,6 +112,5 @@ int countPairs3(int* arr, int len, int value) {
         int ub = upp(arr, i + 1, len - 1, need);
         count += (ub - lb + 1);
     }
-    
     return count;
 }
